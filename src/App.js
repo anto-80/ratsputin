3f57a93ff6b5645f989e412316434f67cc1ee6ed
anto-80 <amwawe@gmail.com>
2021-12-10 14:51:03 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import  React from 'react';
-import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import { useState, useEffect } from 'react';
+import ReactMapGL, {Marker, Popup} from 'react-map-gl';
+import * as parkData from "./data/skateboard-parks.json"
 
 import './App.css';
 
@@ -8,18 +9,67 @@ function App() {
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vh",
-    latitude: 40.730610,
-    longitude:  -73.935242,
+    latitude: 45.4211,
+    longitude:  -75.6903,
     zoom: 8
   });
-  return (
 
+  const [selectedPark, setSelectedPark] = useState(null);
+
+  useEffect(() => {
+    const listener = e => {
+      if (e.key === "Escape") {
+        setSelectedPark(null);
+      }
+    };
+    window.addEventListener("keydown", listener);
+
+    return () => {
+      window.removeEventListener("keydown", listener);
+    };
+  }, []);
+
+  return (
+    <div>
     <ReactMapGL
     {...viewport}
     mapboxApiAccessToken={"pk.eyJ1IjoiYW50bzIyIiwiYSI6ImNrd3dteGF1MDA1Nm4ydXJyZmczbDBzeGgifQ._jesruIOH4dRx5t8cjKAJA"}
-    onViewportChange={nextViewport => setViewport(nextViewport)}
-  />
-    
+     mapStyle="mapbox://styles/mapbox/dark-v10"
+    onViewportChange={viewport => setViewport(viewport)}
+    >
+  
+    {parkData.features.map(park => (
+      <Marker key={park.properties.PARK_ID}
+      latitude={park.geometry.coordinates[1]}  
+      longitude={park.geometry.coordinates[0]}
+      >
+        <button  className="marker-btn"
+              onClick={e => {
+                e.preventDefault();
+                setSelectedPark(park);
+              }}>
+          <img src="/skateboard.png" alt="Skate park icon"/>
+        </button>
+      </Marker>
+    ))}
+
+{selectedPark ? (
+          <Popup
+            latitude={selectedPark.geometry.coordinates[1]}
+            longitude={selectedPark.geometry.coordinates[0]}
+            onClose={() => {
+              setSelectedPark(null);
+            }}
+          >
+            <div>
+              <h2>{selectedPark.properties.NAME}</h2>
+              <p>{selectedPark.properties.DESCRIPTION}</p>
+              <p>{selectedPark.properties.ADDRESS}</p>
+            </div>
+          </Popup>
+        ) : null}
+    </ReactMapGL>
+    </div>
   );
 }
 
